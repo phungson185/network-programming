d8ed81b07dd097dbfeca6a308955b12e999233c4
phungson185 <phungson185@gmail.com>
2021-10-12 22:48:16 +0700

    feat: build echoServer

---
+++ week2/src/controller.h
@@ -0,0 +1,19 @@
+#ifndef __CONTROLLER_H__
+#define __CONTROLLER_H__
+
+void login();
+void menu();
+void schedule(char* stdID);
+void printSchedule(char* stdID);
+char *convertWeekDayToString(int weekDay);
+char *convertSessionToString(int session);
+char *sendToClient(char *buf);
+
+#define MAXLINE 4096   /*max text line length*/
+#define SERV_PORT 3000 /*port*/
+#define LISTENQ 8      /*maximum number of client connections */
+
+int connfd;
+char buf[MAXLINE];
+
+#endif
